bc271fc7b76f5450bafba3724824043d2ba4c2a7
MHiroshiNow <g2154758@cc.kyoto-su.ac.jp>
2025-05-23 11:01:05 +0900

    update: IsArchiveFile関数を追加し、アーカイブファイル判定のテストを実装

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,11 +1,32 @@
 package main
 
-import "fmt"
+import "strings"
 
-func main() {
-	fmt.Println(hello())
-}
-
-func hello() string {
-	return "Hello World!"
+// 概要:
+//
+//	指定されたファイルパスがアーカイブファイルかどうかを判定する関数。
+//
+// 主な仕様:
+//   - .tar, .tar.gz, .tar.bz2, .zip, .war, .ear のいずれかの拡張子であればtrueを返す。
+//
+// 制限事項:
+//   - 拡張子の追加・削除はtargetsスライスを編集することで対応可能。
+//
+// TODO:
+//   - 拡張子の大文字小文字対応。
+//   - より多くのアーカイブ形式への対応。
+//
+// FIXME:
+//   - パスが空文字や不正な場合のエラーハンドリング。
+func IsArchiveFile(path string) bool {
+	targets := []string{
+		".tar", ".tar.gz", ".tar.bz2",
+		".zip", ".war", ".ear",
+	}
+	for _, suffix := range targets {
+		if strings.HasSuffix(path, suffix) {
+			return true
+		}
+	}
+	return false
 }
--- cmd/main/main_test.go
+++ cmd/main/main_test.go
@@ -1,16 +1,49 @@
+// 概要:
+//
+//	IsArchiveFile関数のテーブル駆動テスト。
+//
+// 主な仕様:
+//   - さまざまな拡張子のファイル名に対してアーカイブ判定の真偽を検証する。
+//
+// 制限事項:
+//   - テストケースは必要に応じて追加可能。
+//
+// TODO:
+//   - 増加する拡張子や異常系のテスト追加。
+//
+// FIXME:
+//   - テスト失敗時の出力改善。
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-func Example_main() {
-	main()
-	// Output:
-	// Hello World!
+func TestIsArchiveFile(t *testing.T) {
+	testCases := []struct {
+		given string // 入力ファイル名
+		want  bool   // 期待値
+	}{
+		// テストケースはここに追加
+	}
+
+	for _, tc := range testCases {
+		got := IsArchiveFile(tc.given)
+		if got != tc.want {
+			t.Errorf("IsArchiveFile(%q) = %v, want %v", tc.given, got, tc.want)
+		}
+	}
 }
 
-func Test_hello(t *testing.T) {
-	want := "Hello World!"
-	if got := hello(); got != want {
-		t.Errorf("hello() = %q, want %q", got, want)
-	}
+func ExampleIsArchiveFile() {
+	fmt.Println(IsArchiveFile("hoge.tar"))
+	fmt.Println(IsArchiveFile("hoge.tar.gz"))
+	fmt.Println(IsArchiveFile("hoge.zip"))
+	fmt.Println(IsArchiveFile("hoge.gz"))
+	// Output:
+	// true
+	// true
+	// true
+	// false
 }
